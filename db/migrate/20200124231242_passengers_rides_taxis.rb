54cc2bb659baf7776a36e74bc6ffed9a30bc5f08
sydnee510 <sbsydnee161416@gmail.com>
2020-01-24 15:38:54 -0800

    Done.

---
+++ db/migrate/20200124231242_passengers_rides_taxis.rb
@@ -0,0 +1,8 @@
+class PassengersRidesTaxis < ActiveRecord::Migration[5.0]
+
+    def change
+      add_column :rides, :taxi_id, :integer
+      add_column :rides, :passenger_id, :integer
+  
+  end
+end
